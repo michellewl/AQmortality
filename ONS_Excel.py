bc4db8fc59e7bc921e53e6357ae7253eb62d40f3
Michelle Wan <mwlw3@cam.ac.uk>
2021-04-22 12:39:02 +0100

    Moved Excel sheet details into separate python file.

---
+++ ONS_Excel.py
@@ -0,0 +1,41 @@
+# Weekly mortality Excel files
+
+weekly_urls = {2021:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2021/publishedweek07202101032021165557.xlsx",
+       2020:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2020/publishedweek532020.xlsx",
+       2019:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2019/publishedweek522019.xls",
+       2018:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2018/publishedweek522018withupdatedrespiratoryrow.xls",
+       2017:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2017/publishedweek522017.xls",
+       2016:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2016/publishedweek522016.xls",
+       2015:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2015/publishedweek2015.xls",
+       2014:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2014/publishedweek2014.xls",
+       2013:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2013/publishedweek2013.xls",
+       2012:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2012/publishedweek2012.xls",
+       2011:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2011/publishedweek2011.xls",
+       2010:"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2010/publishedweek2010.xls"
+       }
+
+weekly_sheet_names = {2021:f"Weekly figures 2021",
+            2020:f"Weekly figures 2020",
+            2019:f"Weekly figures 2019",
+            2018:f"Weekly figures 2018",
+            2017:f"Weekly figures 2017",
+            2016:f"Weekly figures 2016",
+            2015:f"Weekly Figures 2015",
+            2014:f"Weekly Figures 2014",
+            2013:f"Weekly Figures 2013",
+            2012:f"Weekly Figures 2012",
+            2011:f"Weekly Figures 2011",
+            2010:f"Weekly Figures 2010"}
+
+weekly_row_indices = {2021:[5, 88],
+              2020:[5, 92],
+              2019:[3, 47],
+              2018:[3, 47],
+              2017:[3, 47],
+              2016:[3, 47],
+              2015:[3, 47],
+              2014:[2, 47],
+              2013:[3, 47],
+              2012:[3, 47],
+              2011:[3, 48],
+              2010:[3, 47]}
