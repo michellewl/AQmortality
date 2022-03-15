4afc515e813993eae2199db1730c9f831f13869c
Michelle Wan <mwlw3@cam.ac.uk>
2022-03-15 13:40:19 +0000

    Code to combine the LAQN station locations with GDHI data, and plot with colour scaled map of London.

--- ADMS_functions.py
+++ ADMS_functions.py
@@ -42,8 +42,8 @@ def PointXYZ_to_latlon(PointXs, PointYs):
     df.reset_index(drop=True, inplace=True)
     return df
 
-def plot_on_map(data_geodataframe, map_geodataframe, column=None, title="Greater London", fontsize="25", figsize=(20,10), data_color=None, data_cmap=None, colorbar=False, set_colorbar_max=False, set_colorbar_log=False, data_markersize=0.1, map_color="whitesmoke", map_edge_color="black", axis="off"):
-    data_geodataframe.plot(column=column, ax=map_geodataframe.plot(figsize=figsize, color=map_color, edgecolor=map_edge_color), color=data_color, cmap=data_cmap, markersize=data_markersize)
+def plot_on_map(data_geodataframe, map_geodataframe, column=None, map_column=None, map_cmap=None, title="Greater London", fontsize="25", figsize=(20,10), data_color=None, data_cmap=None, colorbar=False, map_colorbar=False, set_colorbar_max=False, set_map_colorbar_max=False, set_colorbar_log=False, set_map_colorbar_log=False, data_markersize=0.1, map_color="whitesmoke", map_edge_color="black", axis="off"):
+    data_geodataframe.plot(column=column, ax=map_geodataframe.plot(column=map_column, figsize=figsize, color=map_color, edgecolor=map_edge_color, cmap=map_cmap), color=data_color, cmap=data_cmap, markersize=data_markersize)
     if colorbar:
         if set_colorbar_max:
             colorbar_max = set_colorbar_max
@@ -55,6 +55,17 @@ def plot_on_map(data_geodataframe, map_geodataframe, column=None, title="Greater
             norm = plt.Normalize(data_geodataframe[column].min(), colorbar_max)
         plt.colorbar(plt.cm.ScalarMappable(cmap=data_cmap, 
         norm=norm)).set_label(column)
+    if map_colorbar:
+        if set_colorbar_max:
+            colorbar_max = set_map_colorbar_max
+        else:
+            colorbar_max = map_geodataframe[map_column].max()
+        if set_map_colorbar_log:
+            norm = colors.LogNorm(map_geodataframe[map_column].quantile(0.01), colorbar_max)
+        else:
+            norm = plt.Normalize(map_geodataframe[map_column].min(), colorbar_max)
+        plt.colorbar(plt.cm.ScalarMappable(cmap=map_cmap, 
+        norm=norm)).set_label(map_column)
     plt.suptitle(title, fontsize=fontsize)
     plt.axis(axis)
     plt.show()
