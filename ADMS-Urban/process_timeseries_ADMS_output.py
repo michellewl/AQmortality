95ae7bba6e7da08290ee35fd415e9321844eaca4
Michelle Wan <mwlw3@cam.ac.uk>
2022-05-16 16:59:12 +0100

    Processing each region individually first.

--- ADMS-Urban/process_timeseries_ADMS_output.py
+++ ADMS-Urban/process_timeseries_ADMS_output.py
@@ -18,10 +18,19 @@ folder = f"/home/users/mwlw3/ADMS-Urban/2019_hourly_met/all_regions/{run}/"
 files = [path.join(folder, file) for file in listdir(folder) if path.splitext(file)[-1]==".nc"]
 processed_coordinates_filepath = path.join(folder, "raw_processed_coordinates.nc")
 
+# Processing from raw ADMS-Urban outputs to a netCDF file with useful attributes and latitude/longitude coordinates
+
 if not path.exists(processed_coordinates_filepath):
     print(f"Loading raw data for run {run} and processing the netCDF coordinates...")
-    new_ds = xr.concat([process_timeseries_dataset(xr.open_dataset(files[i])) for i in tqdm(range(len(files)))], "space")
-    new_ds.to_netcdf(processed_coordinates_filepath)
+    makedirs(path.join(folder, "raw_processed_regions"))
+    for i in tqdm(range(len(files))):
+        region = files[i].split(".")[-2]
+        region_filepath = path.join(folder, "raw_processed_regions", f"{region}.nc")
+        region_ds = process_timeseries_dataset(xr.open_dataset(files[i]))
+        region_ds.to_netcdf(region_filepath)
+    # loop through processed region files... for 
+     #new_ds = xr.concat([ ], "space")
 elif path.exists(processed_coordinates_filepath):
         new_ds = xr.open_dataset(processed_coordinates_filepath)
         print(f"Loaded the processed coordinate data for run {run}.")
+
