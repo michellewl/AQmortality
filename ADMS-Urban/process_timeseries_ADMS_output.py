8ca1aaed7911f0499d0ab5945908c797459867c2
Michelle WL Wan <54372792+michellewl@users.noreply.github.com>
2022-05-16 17:53:31 +0100

    Add missing brackets.

--- ADMS-Urban/process_timeseries_ADMS_output.py
+++ ADMS-Urban/process_timeseries_ADMS_output.py
@@ -14,7 +14,8 @@ from PIL import Image
 from ADMS_functions import extract_dataset_time_axis, plot_timeseries, PointXYZ_to_latlon, plot_on_map, plot_in_grid_box, process_timeseries_dataset
 
 run = "015"
-folder = f"/home/users/mwlw3/ADMS-Urban/2019_hourly_met/all_regions/{run}/"
+# folder = f"/home/users/mwlw3/ADMS-Urban/2019_hourly_met/all_regions/{run}/"
+folder = f"C:Users\mwlw3\OneDrive - University of Cambridge\Documents\Prelim_runs\2019_hourly_met\all_regions\{run}"
 files = [path.join(folder, file) for file in listdir(folder) if path.splitext(file)[-1]==".nc"]
 processed_coordinates_filepath = path.join(folder, "raw_processed_coordinates.nc")
 
@@ -32,12 +33,12 @@ if not path.exists(processed_coordinates_filepath):
             region_ds = process_timeseries_dataset(xr.open_dataset(files[i]))
             region_ds.to_netcdf(region_filepath)
     # loop through processed region files... 
-    print("Concatenating netCDF files..."
+    print("Concatenating netCDF files...")
     new_ds = xr.concat([xr.open_dataset(path.join(processed_regions_folder, listdir(processed_regions_folder)[i])) for i in tqdm(range(len(listdir(processed_regions_folder))))], "space")
    
-    print("Saving new netCDF file..."
+    print("Saving new netCDF file...")
     new_ds.to_netcdf(processed_coordinates_filepath)
-    print(f"Saved to {processed_coordinates_filepath}"
+    print(f"Saved to {processed_coordinates_filepath}")
 
 elif path.exists(processed_coordinates_filepath):
     new_ds = xr.open_dataset(processed_coordinates_filepath)
